299b27900836ee38226d434fd9c9ded615b7e485
kimiko88 <chimien@libero.it>
2020-03-09 15:07:23 +0100

    first commit

---
+++ request.js
@@ -0,0 +1,86 @@
+
+const http = require('http')
+const https = require('https')
+const querystring = require('querystring');
+
+const requests = {
+  getContent(url, method = 'GET', headers = {}, bodyData = null) {
+    return new Promise((resolve, reject) => {
+      const lib = url.startsWith('https') ? https : http
+      const urlObj = new URL(url)
+      if (bodyData) {
+        bodyData = requests.setContent(headers, bodyData)
+      }
+      const request = lib.request({
+        headers,
+        path: `${urlObj.pathname}${urlObj.search}`,
+        host: urlObj.hostname,
+        port: urlObj.port,
+        method
+      }, async (response) => {
+        if (response.statusCode < 200 || response.statusCode > 299) {
+          if (response.statusCode === 301) {
+            request.end()
+            resolve(await requests.redirect(response, method, headers))
+            return
+          } else {
+            console.log(`Failed to load page, status code: ${response.statusCode}`)
+          }
+        }
+        const bodyChunks = []
+        response.on('data', chunk => bodyChunks.push(chunk))
+        response.on('end', () => {
+          const body = Buffer.concat(bodyChunks)
+          if (response.headers) {
+            const bodyResponse = body.toString('utf8')
+            if (response.headers['content-type'].startsWith('application/json')) {
+              try {
+                resolve({ data: JSON.parse(bodyResponse), headers: response.headers, code: response.statusCode })
+              } catch (err) {
+                console.error(err)
+                resolve(undefined)
+              }
+            } else {
+              resolve({ data: bodyResponse, headers: response.headers, code: response.statusCode })
+            }
+          }
+        })
+      })
+      request.on('error', (err) => {
+        console.error(err)
+        reject(err)
+      })
+      if (bodyData) {
+        request.write(bodyData)
+      }
+      request.end()
+    })
+  },
+
+  setContent(headers, bodyData) {
+    if (headers['content-type'] === "application/x-www-form-urlencoded" || headers['Content-Type'] === "application/x-www-form-urlencoded") {
+      var form = querystring.stringify(bodyData)
+      headers['content-length'] = form.length
+      return form
+    } else if ((headers['content-type'] && headers['content-type'].startsWith('application/json')) || (headers['Content-Type'] && headers['Content-Type'].startsWith('application/json'))|| (headers['Content-type'] && headers['Content-type'].startsWith('application/json'))) {
+      return JSON.stringify(bodyData)
+    }
+    return bodyData
+  },
+
+  async redirect(response, method, headers) {
+    console.log(`Redirect to ${response.headers.location}`)
+    var redirectUrl = response.headers.location
+    if (redirectUrl && (redirectUrl.indexOf('http://') !== 0 || redirectUrl.indexOf('https://') !== 0)) {
+      return await this.getContent(`${response.headers.location}`, method, headers)
+    }
+    return undefined
+  },
+
+  async getSize(url) {
+    const result = await this.getContent(url, 'HEAD')
+    return result.headers ? result.headers['content-length'] : undefined
+  }
+}
+
+module.exports = requests
---
+++ test/test.js
@@ -0,0 +1,64 @@
+const assert = require('assert')
+const request = require('../request')
+
+describe('request', () => {
+    it('send get request', async () => {
+        var res = await request.getContent('https://google.com')
+        assert.notEqual(res.data, undefined, "data is undefined")
+        assert.notEqual(res.headers, undefined)
+        assert.equal(res.code, 200)
+    })
+
+    it('send get request to fantasy addres', async () => {
+        var res = await request.getContent('https://www.google.com/prova2542245442255225')
+        assert.notEqual(res.data, undefined)
+        assert.notEqual(res.headers, undefined)
+        assert.equal(res.code, 404)
+    })
+
+    it('get json', async () => {
+        var res = await request.getContent('https://jsonplaceholder.typicode.com/posts/1')
+        assert.notEqual(res.data, undefined)
+        assert.notEqual(res.data.body, undefined)
+        assert.notEqual(res.headers, undefined)
+        assert.equal(res.code, 200)
+    })
+
+    it('post json', async () => {
+        var res = await request.getContent('http://dummy.restapiexample.com/api/v1/create', 'POST', {
+            "Content-type": "application/json; charset=UTF-8"
+        }, {
+            name: "foo",
+            salary: "bar",
+            age: 12
+        })
+        assert.notEqual(res.data, undefined)
+        assert.notEqual(res.data.data.name, undefined)
+        assert.notEqual(res.headers, undefined)
+        assert.equal(res.code, 200)
+    })
+
+    it('put json', async () => {
+        var res = await request.getContent('http://dummy.restapiexample.com/api/v1/update/21', 'PUT', {
+            "Content-type": "application/json; charset=UTF-8"
+        }, {
+            name: "foo",
+            salary: "bar",
+            age: 15
+        })
+        assert.notEqual(res.data, undefined)
+        assert.equal(res.data.data, null)
+        assert.notEqual(res.headers, undefined)
+        assert.equal(res.code, 200)
+    })
+
+    it('delete json', async () => {
+        var res = await request.getContent('http://dummy.restapiexample.com/api/v1/delete/2', 'DELETE')
+        assert.notEqual(res.data, undefined)
+        assert.equal(res.data.data, null)
+        assert.notEqual(res.headers, undefined)
+        assert.equal(res.code, 200)
+    })
+
+
+})
